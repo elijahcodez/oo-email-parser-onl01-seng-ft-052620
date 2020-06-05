6c8c04282742bbd74313b91f1b46b2283d047dcf
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-05 20:02:18 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,7 +1,14 @@
 class EmailAddressParser
-  attr_accessor :emails 
+attr_accessor :emails 
 
-  def initialize(emails)
+def initialize(emails)
   @emails = emails
-  end 
+end 
+
+def parse
+  puts splitEmail = emails.split.collect {|address| address.split(',')} 
+  returnArr = splitEmail.flatten.uniq
+  returnArr
+end
+
 end
