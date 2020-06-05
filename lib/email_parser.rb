3deda6b7bd87ed36fd22c5a381e1294de15821c9
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-05 19:53:40 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 class EmailParser
-	attr_accessor :emails
-	
-	def initialize(emails)
-		@emails= emails
-	end
-	
-	def parse
-		@emails = @emails.gsub(/[\s,]/ ," ").split
-		# @email is an array now
-		@emails.uniq
-	end
-	
+
+  attr_accessor :emails
+
+  def initialize(emails)
+    @emails = emails
+  end
+
+  def parse
+    email_array = []
+    email_array = @emails.split(/[,\s]/)
+    email_array = email_array.reject { |e| e == ""}
+    email_array.uniq
+  end
+
 end
+
+emails = "hpotter@hogwarts.org, he_who_must_not_be_named@riddleshouse.net"
+parser = EmailParser.new(emails)
+
+puts parser.parse
